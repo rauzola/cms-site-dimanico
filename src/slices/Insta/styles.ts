29531b9c1bdd3879873e9f50ba762b151d57a149
Raul Sigoli <raul_sigoli@hotmail.com>
2024-09-04 13:15:19 -0300

    feat:  remove min-height

--- src/slices/Insta/styles.ts
+++ src/slices/Insta/styles.ts
@@ -6,7 +6,7 @@ interface IColor {
 
 export const Container = styled.div`
   width: 100vw;
-  min-height: 100vh;
+  
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
