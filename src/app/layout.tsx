4489abd491f79644839a0716ff4852d8348ca7ea
Raul Sigoli <raul_sigoli@hotmail.com>
2024-09-05 17:01:49 -0300

    feat: add menu novo funcionando

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
-
 import "./globals.css";
 
-import { Inter } from "next/font/google";
-import { PrismicPreview } from "@prismicio/next";
-
-import { repositoryName } from "@/prismicio";
 import Header from "@/components/Header";
+import { createClient } from "@/prismicio";
+import { PrismicPreview } from "@prismicio/next";
+import { Inter } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,17 +11,30 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+  const navigation = await client.getSingle("navigation");
+
+  // Prepare the data to be passed as props
+  const headerProps = {
+    logoName: settings.data?.logo_nome || "Default Logo", // Use um valor padrão se logo_nome não estiver disponível
+    logoImage: settings.data?.imgaem_logo.url,
+    navigationLinks: navigation.data?.links.map((item: any) => ({
+      label: item.label,
+      link: item.link
+    })) || []
+  };
+
 
   
   return (
     <html lang="pt-BR" className={inter.variable}>
       <body className="overflow-x-hidden antialiased">
-        <Header />
+        <Header {...headerProps} />
         {children}
-        <PrismicPreview repositoryName={repositoryName} />
+        <PrismicPreview repositoryName="your-repo-name" />
       </body>
     </html>
   );
 }
-
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,82 +1,90 @@
 "use client"; // Adiciona esta linha no topo do arquivo
 
+import { PrismicNextLink } from '@prismicio/next';
+import { PrismicText } from '@prismicio/react';
 import { useState } from 'react';
 
-export default function Header() {
+interface Link {
+  label: string;
+  link: any; // Ajuste o tipo conforme necessário
+}
+
+interface HeaderProps {
+  logoName: string;
+  navigationLinks: Link[];
+  logoImage: string | any; // Ajuste o tipo conforme necessário
+}
+
+export default function Header({ logoName, navigationLinks, logoImage }: HeaderProps) {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   // Alternar visibilidade do menu suspenso
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
-  
+
   // Alternar visibilidade do menu móvel
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
+
+  console.log(logoImage, "navigationLinks")
+
+
+
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
-        <button
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-dropdown"
-          aria-expanded={isMenuOpen}
-          onClick={toggleMenu}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-          </svg>
-        </button>
-        <div className={`relative w-full md:w-auto ${isMenuOpen ? 'block' : 'hidden'} md:block`} id="navbar-dropdown">
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
-            </li>
-            <li className="relative">
-              <button
-                type="button"
-                onClick={toggleDropdown}
-                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-              >
-                Dropdown
-                <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-                </svg>
-              </button>
-              {/* Dropdown menu */}
-              <div className={`absolute left-0 mt-2 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow z-20 ${isDropdownOpen ? 'block' : 'hidden'} dark:bg-gray-700 dark:divide-gray-600`}>
-                <ul className="py-2 text-sm text-gray-700 dark:text-gray-400">
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                  </li>
-                </ul>
-                <div className="py-1">
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                </div>
-              </div>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-            </li>
-          </ul>
+    <>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          {/* Logo e Título */}
+          <a href="/home" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={logoImage} className="h-16 w-16 rounded" alt="Flowbite Logo" />  {/*  borda da imagem  */}
+
+            {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              {logoName}
+            </span> */}
+
+          </a>
+
+
+          {/* Botão do Menu Móvel */}
+          <button
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-dropdown"
+            aria-expanded={isMenuOpen}
+            onClick={toggleMenu}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
+
+          {/* Menu de Navegação */}
+          <div className={`relative w-full md:w-auto ${isMenuOpen ? 'block' : 'hidden'} md:block`} id="navbar-dropdown">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse 
+              md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+
+              {/* Item do Menu: Home */}
+
+
+
+              {/* Itens do Menu */}
+              {navigationLinks.map((item: any, index: number) => (
+                <PrismicNextLink
+                  key={index}
+                  field={item.link}
+                  className="block px-2  hover:bg-gray-100 "
+                >
+                  <PrismicText field={item.label} />
+                </PrismicNextLink>
+              ))}
+
+
+
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
